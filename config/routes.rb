d4cf1c7eada470ed83e0ac479e60ef7c7083f070
Yan <yanfernandes@gmail.com>
2021-03-09 21:14:48 -0300

    index page twitter accounts

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,12 @@ Rails.application.routes.draw do
 
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
-
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
   get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
 
   root to: "main#index"
 
